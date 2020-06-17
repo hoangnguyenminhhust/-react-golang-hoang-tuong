30ee8577251e67cc17409bfbbc9426f12f6def7b
Nguyễn Minh Hoàng <hoangnguyenminh.hust@gmail.com>
2020-06-17 19:49:18 +0700

    update api

--- api/main.go
+++ api/main.go
@@ -1,36 +1,127 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+var collection *mongo.Collection
+
+// Person represents a person document in MongoDB
+type News struct {
+	ID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name    string             `json:"name,omitempty" bson:"name,omitempty"`
+	Content string             `json:"content,omitempty" bson:"content,omitempty"`
+	Author  string             `json:"description,omitempty" bson:"description,omitempty"`
+	Image   string             `json:"image,omitempty" bson:"image,omitempty"`
+	Title   string             `json:"title,omitempty" bson:"title,omitempty"`
+}
+
+func createNews(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var news News
+	json.NewDecoder(request.Body).Decode(&news)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, _ := collection.InsertOne(ctx, news)
+	id := result.InsertedID
+	news.ID = id.(primitive.ObjectID)
+
+	json.NewEncoder(response).Encode(news)
+
+}
+
+func getNewByID(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var news News
+	id, _ := primitive.ObjectIDFromHex(mux.Vars(request)["id"])
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err := collection.FindOne(ctx, News{ID: id}).Decode(&news)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
 		return
 	}
-	switch r.Method {
-	case "GET":
-		for k, v := range r.URL.Query() {
-			fmt.Printf("%s: %s\n", k, v)
-		}
-		w.Write([]byte("Received a GET request\n"))
-	case "POST":
-		reqBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%s\n", reqBody)
-		w.Write([]byte("Received a POST request\n"))
-	default:
-		w.WriteHeader(http.StatusNotImplemented)
-		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
-	}
+	json.NewEncoder(response).Encode(news)
 }
+
+func getListNews(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var ListNews []News
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var news News
+		cursor.Decode(&news)
+		ListNews = append(ListNews, news)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(ListNews)
+}
+
+func updateNews(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var news News
+	id, _ := primitive.ObjectIDFromHex(mux.Vars(request)["id"])
+	json.NewDecoder(request.Body).Decode(&news)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, err := collection.UpdateOne(ctx, News{ID: id}, bson.M{"$set": news})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(result)
+
+}
+
+func deleteNews(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	id, _ := primitive.ObjectIDFromHex(mux.Vars(request)["id"])
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, err := collection.DeleteOne(ctx, News{ID: id})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(result)
+}
+
 func main() {
-	http.HandleFunc("/", helloWorld)
-	http.ListenAndServe(":8000", nil)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ := mongo.Connect(ctx, clientOptions)
+	collection = client.Database("golang").Collection("news")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", getListNews).Methods("GET")
+	router.HandleFunc("/{id}", getNewByID).Methods("GET")
+	router.HandleFunc("/", createNews).Methods("POST")
+	router.HandleFunc("/{id}", updateNews).Methods("PUT")
+	router.HandleFunc("/{id}", deleteNews).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
